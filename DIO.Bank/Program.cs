e3f4b92373dc8e24b4cce6e3dee06efe88dd54ec
Rodrigo Medeiros <rrodrigomedeiros@gmail.com>
2021-09-28 23:39:33 -0300

    Implementada os metodos de Listar e Inserir contas. Removendo arquivos desnecessário

--- DIO.Bank/Program.cs
+++ DIO.Bank/Program.cs
@@ -1,14 +1,111 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DIO.Bank
 {
     class Program
     {
+        static List<Conta> listContas = new List<Conta>();
         static void Main(string[] args)
         {
-            Conta minhaConta = new Conta(TipoDeConta.PessoaFisica,0,0,"Esferico");
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarContas();
+                        break;
+                    
+                    case "2":
+                        InserirConta();
+                        break;
+
+                    case "3":
+                        // Tranferir();
+                        break;
+
+                    case "4":
+                        // Sacar();
+                        break;
+
+                    case "5":
+                        // Depositar()
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+            Console.WriteLine("Obrigado por utilizar nossos serviços.");
+            Console.ReadLine();
+        }
+
+        private static void InserirConta()
+        {
+            Console.WriteLine("Inserir nova conta");
             
-            Console.WriteLine(minhaConta.ToString());
+            Console.Write("Digite 1 para Conta Fisica ou 2 para Juridica: ");
+            int entradaTipoConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do cliente: ");
+            string entradaNome = Console.ReadLine();
+
+            Console.Write("Digite o saldo inicial: ");
+            double entradaSaldo = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o credito: ");
+            double entradaCredito = double.Parse(Console.ReadLine());
+
+            Conta novaConta = new Conta(tipoConta:(TipoDeConta)entradaTipoConta,
+                                                     saldo: entradaSaldo,
+                                                     credito: entradaCredito,
+                                                     nome: entradaNome);
+            listContas.Add(novaConta);
+
+        }
+
+        private static void ListarContas()
+        {
+            Console.WriteLine("Listar contas");
+            if (listContas.Count == 0)
+            {
+                Console.WriteLine("Nenhuma conta cadastrada.");
+                return;
+            }
+
+            for (int i = 0; i < listContas.Count; i++)
+            {
+                Conta conta = listContas[i];
+                Console.Write("${0} - ",i);
+                Console.WriteLine(conta);
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Dio Bank a seu dispor!");
+            Console.WriteLine("Informe a opção desejada:");
+
+            Console.WriteLine("1 - Listar contas");
+            Console.WriteLine("2 - Inserir nova conta");
+            Console.WriteLine("3 - Transferir");
+            Console.WriteLine("4 - Sacar");
+            Console.WriteLine("5 - Depositar");
+            Console.WriteLine("C - Limpar tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
